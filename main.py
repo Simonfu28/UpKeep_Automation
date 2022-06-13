ffddc5e7ac5e12926813c126bf84ae6f5ccce20a
Simon <simonfu28@gmail.com>
2022-06-13 13:04:24 -0700

    Added auth token and test get script

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import requests
 import modules
 
 
 if __name__ == '__main__':
-    x = modules.auth('simon.fu@avcorp.com', 'Upkeep2021')
-    print(x)
+    ##x = modules.auth('simon.fu@avcorp.com', 'Upkeep2021')
+    ##token = x 
+    wo = modules.test("r:d016cc30960d50f625df341c9f43a2d4")
+
 
--- modules.py
+++ modules.py
@@ -5,7 +5,8 @@ import json
 accepted_code = [200, 201, 202, 203, 204, 205, 206]
 
 def auth(user, password):
-    r = requests.post("https://api.onupkeep.com/api/v2/auth", data={'email':user, 'password':password})
+    r = requests.get("https://api.onupkeep.com/api/v2/auth", data={'email':user, 'password':password})
+    print(r.text)
         
     if r.status_code in accepted_code:
         temp = json.loads(r.text)
@@ -13,5 +14,12 @@ def auth(user, password):
         token = temp["sessionToken"]
         return token
     else:
-        temp = "Error: Code " + r.status_code
+        temp = "Error: Code " + str(r.status_code)
         return temp
+        ## Throw error here
+    
+def test(token):
+    payload = {"id": "O1L94DYjCv"}
+
+    r = requests.get("https://api.onupkeep.com/api/v2/work-orders/", params=payload, headers={"Session-Token": token})
+    print(r.status_code, r.text)
